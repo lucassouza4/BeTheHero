7d9cbc6a4254302f64af2b362f621dc34e6bf929
Lucas Souza <lucassilva1998@oi.com.br>
2020-07-07 16:10:22 -0300

    FrontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './global.css';
+import Routes from './routes';
+
+function App() {
+  // const [contador,setContador] = useState(0); // array [valor,funçao de troca]
+
+  // function increment(){
+  //   setContador(contador + 1);
+  // }
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
